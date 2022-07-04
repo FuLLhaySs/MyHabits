916833079a50187bc06c8bfafd127dfcc0fcd1db
FuLLhaySs <fullhays@icloud.com>
2022-07-04 22:09:10 +0300

    LaunchScreen

---
+++ MyHabits/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  MyHabits
+//
+//  Created by Игорь Павлов on 04.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor.blue
+    }
+}
+
